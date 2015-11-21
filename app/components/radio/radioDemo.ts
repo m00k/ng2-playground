51c254930e93b3da17db18fa950e278f970e1207
m00k <christian.bican@gmail.com>
2015-11-21 19:20:52 +0100

    just a hint of styling...

--- app/components/radio/radioDemo.ts
+++ app/components/radio/radioDemo.ts
@@ -3,28 +3,32 @@ import {Component, CORE_DIRECTIVES, FORM_DIRECTIVES} from 'angular2/angular2';
 
 @Component({
   template: `
-    <h1>Radio Group</h1>
-    <p class="note">
-    TODO: two way data binding (No value accessor for '' in [null] when using ng-model)
-    </p>
+    <div class="container">
+      <h1>Radio Group</h1>
+      <p class="note">
+      TODO: two way data binding (No value accessor for '' in [null] when using ng-model)
+      </p>
 
-    <radio-group [value]="rgVal" (change)="rgVal=$event.target.value" [name]="'group1'">
-      <radio>
-        <input type="radio" value="1"/>
-        <span>radio 1</span>
-      </radio>
-      <radio>
-        <input type="radio" value="2"/>
-        <span>radio 2</span>
-      </radio>
-    </radio-group>
+      <div class="well">
+        <radio-group [value]="rgVal" (change)="rgVal=$event.target.value" [name]="'group1'">
+          <radio>
+            <input type="radio" value="1"/>
+            <span>radio 1</span>
+          </radio>
+          <radio>
+            <input type="radio" value="2"/>
+            <span>radio 2</span>
+          </radio>
+        </radio-group>
+      </div>
 
-    <p class="note">
-    radio group value {{rgVal}}
-    </p>
-    <p class="note">
-    <input type="text" [(ng-model)]="rgVal">
-    </p>
+      <p class="note">
+      radio group value {{rgVal}}
+      </p>
+      <p class="note">
+      <input type="text" [(ng-model)]="rgVal">
+      </p>
+    </div>
   `,
   directives: [FORM_DIRECTIVES, CORE_DIRECTIVES, RadioGroupCmp, RadioInputCmp, RadioCmp]
 })
